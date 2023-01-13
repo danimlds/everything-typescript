e70afe3c43cafde2d643670e37bb1e2816f22bd8
danimlds <daniel.mlds@gmail.com>
2023-01-13 13:45:04 -0300

    typescript fundamenntos

---
+++ 02_fundamentos/index.js
@@ -0,0 +1,26 @@
+"use strict";
+//numbers
+let x = 10;
+console.log(x);
+console.log(typeof (x));
+let pi = 3.151492;
+console.log(pi);
+console.log(pi.toPrecision(6));
+//string
+const firstName = "Jason";
+console.log(firstName.toUpperCase());
+let fullName;
+const lastName = "Bourne";
+fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(typeof (fullName));
+//boolean
+let a = false;
+console.log(a);
+console.log(typeof (a));
+a = true;
+console.log(a);
+// inference e annotation
+let ann = "Teste";
+let inf = "Teste";
+console.log(inf);
---
+++ 02_fundamentos/index.ts
@@ -0,0 +1,49 @@
+//numbers
+let x: number = 10
+
+console.log(x);
+
+console.log(typeof(x));
+
+let pi:number = 3.151492;
+
+console.log(pi);
+
+console.log(pi.toPrecision(6));
+
+
+//string
+
+const firstName: string = "Jason";
+
+console.log(firstName.toUpperCase());
+
+let fullName: string
+
+const lastName = "Bourne";
+
+fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+console.log(typeof(fullName));
+
+//boolean
+
+let a: boolean = false;
+
+console.log(a);
+
+console.log(typeof(a));
+
+a = true;
+
+console.log(a);
+
+// inference e annotation
+
+let ann: string = "Teste"
+
+let inf = "Teste";
+
+console.log(inf)
